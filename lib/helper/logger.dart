8838213000729f80ec09593677e59e22024ad99b
likh27596 <505754686@qq.com>
2024-01-31 14:56:39 +0800

    xxx

---
+++ lib/helper/logger.dart
@@ -0,0 +1,26 @@
+import 'dart:io';
+
+import 'package:askaide/helper/path.dart';
+import 'package:askaide/helper/platform.dart';
+import 'package:logger/logger.dart' as logger;
+
+class Logger {
+  static final logger.Logger instance = logger.Logger(
+    printer: logger.PrettyPrinter(
+      lineLength: 120,
+      printTime: true,
+      colors: false,
+      noBoxingByDefault: true,
+    ),
+    output: logger.MultiOutput(
+      [
+        logger.ConsoleOutput(),
+        if (!PlatformTool.isWeb())
+          logger.FileOutput(
+            file: File(PathHelper().getLogfilePath),
+            overrideExisting: true,
+          ),
+      ],
+    ),
+  );
+}
